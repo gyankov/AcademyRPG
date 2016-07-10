110172adbbe7ef1e5e784bc0057960bb4fb8f003
gyankov <g.yankov951@abv.bg>
2016-07-10 20:09:01 +0300

    Add files via upload

---
+++ 2/AcademyRPG/AcademyRPG/CharacterFighter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+   abstract class CharacterFighter : Character, IFighter
+    {
+        public CharacterFighter(string name, Point position, int owner) : base(name, position, owner)
+        {
+        }
+
+        public int AttackPoints
+        {
+            get;
+        }
+
+        public int DefensePoints
+        {
+            get;
+          
+        }
+
+        public abstract int GetTargetIndex(List<WorldObject> availableTargets);
+       
+    }
+}
---
+++ 2/AcademyRPG/AcademyRPG/Ninja.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    class Ninja : Character, IFighter, IGatherer
+    {
+        private int attackPoints;
+       
+
+        public Ninja(string name, Point position, int owner) : base(name, position, owner)
+        {
+            this.HitPoints = 1;
+            this.attackPoints = 0;
+        }
+
+        public int AttackPoints
+        {
+            get
+            {
+                return this.attackPoints;
+            }
+        }
+            
+        public int DefensePoints
+        {
+            get
+            {
+                return int.MaxValue;
+            }
+        }
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+           IEnumerable<WorldObject> differentOwner = availableTargets.Where(x => x.Owner != 0 && x.Owner != this.Owner);
+            WorldObject mostHitPoints = differentOwner.OrderByDescending(x => x.HitPoints).FirstOrDefault();
+            int result = availableTargets.IndexOf(mostHitPoints);
+
+            return result;
+        }
+
+        public bool TryGather(IResource resource)
+        {
+            if (resource.Type== ResourceType.Lumber)
+            {
+                this.attackPoints += resource.Quantity;
+                return true;
+            }
+            else if (resource.Type==ResourceType.Stone)
+            {
+                this.attackPoints += resource.Quantity * 2;
+                return true;
+            }
+            return false;
+        }
+    }
+}
